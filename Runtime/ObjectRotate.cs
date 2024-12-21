574aabd8d9c04b38a1fcefb22afb690de72103de
LYLibrary <1402609566@qq.com>
2024-12-22 00:57:00 +0800

    2

---
+++ Runtime/ObjectRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour
+{
+    public float angle = 1;
+    void Update()
+    {
+        transform.Rotate(transform.forward, angle);
+    }
+}
