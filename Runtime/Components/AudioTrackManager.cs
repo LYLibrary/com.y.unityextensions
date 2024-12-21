238cdb80adfc480c534390a3678d58b1befccee9
LYLibrary <1402609566@qq.com>
2024-12-22 01:19:06 +0800

    正式进行包内容创建

---
+++ Runtime/Components/AudioTrackManager.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[DefaultExecutionOrder(-1000)]
+public class AudioTrackManager : MonoBehaviour
+{
+    private void Reset()
+    {
+        gameObject.name = "[音轨管理器]";
+        transform.position = Vector3.zero;
+    }
+
+    private static AudioTrackManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (!instance.gameObject.Equals(gameObject))
+            {
+                gameObject.SetActive(false);
+                DestroyImmediate(gameObject);
+            }
+        }
+    }
+
+    [SerializeField, Header("音轨")] private List<AudioTrack> audioTracks = new List<AudioTrack>();
+
+    public static void CreateAudioTrack(string trackName)
+    {
+        if (string.IsNullOrEmpty(trackName)) { return; }
+        AudioTrack findTrack = instance.audioTracks.Find(x => x.Name == trackName);
+        if (findTrack != null) { return; }
+        GameObject audioTrack = new GameObject(trackName, typeof(AudioSource));
+        audioTrack.transform.SetParent(instance.transform);
+        audioTrack.transform.position = Vector3.zero;
+        audioTrack.transform.eulerAngles = Vector3.zero;
+        AudioSource source = audioTrack.GetComponent<AudioSource>();
+        source.playOnAwake = false;
+        AudioTrack track = new AudioTrack(trackName, source);
+        instance.audioTracks.Add(track);
+    }
+
+    public static void DeleteAudioTrack(string trackName)
+    {
+        AudioTrack findTrack = instance.audioTracks.Find(x => x.Name == trackName);
+        if (findTrack != null)
+        {
+            instance.audioTracks.Remove(findTrack);
+            findTrack.Delete();
+        }
+    }
+
+    public static void DeleteAllAudioTrack()
+    {
+        List<AudioTrack> tempList = new List<AudioTrack>(instance.audioTracks);
+        instance.audioTracks.Clear();
+        for (int i = 0; i < tempList.Count; i++)
+        {
+            tempList[i].Delete();
+        }
+    }
+
+    public static void StopAllAudioTrack()
+    {
+        for (int i = 0; i < instance.audioTracks.Count; i++)
+        {
+            instance.audioTracks[i].Stop();
+        }
+    }
+
+    public static void SetAudioClip(string trackName, AudioClip clip)
+    {
+        AudioTrack findTrack = instance.audioTracks.Find(x => x.Name == trackName);
+        if (findTrack != null)
+        {
+            findTrack.clip = clip;
+        }
+    }
+
+    public static AudioTrack GetAudioTrack(string trackName)
+    {
+        AudioTrack findTrack = instance.audioTracks.Find(x => x.Name == trackName);
+        return findTrack;
+    }
+
+    [Serializable]
+    public class AudioTrack
+    {
+        [SerializeField] private string name;
+        [SerializeField] private AudioSource source;
+        private AudioTrackListener trackListener;
+
+        public AudioTrack(string name, AudioSource source)
+        {
+            this.name = name;
+            this.source = source;
+            trackListener = this.source.gameObject.AddComponent<AudioTrackListener>();
+            trackListener.finishOne = () => { finishOne?.Invoke(); };
+        }
+
+        public string Name { get { return name; } }
+        public bool Mute { get { return source.mute; } set { source.mute = value; } }
+        public bool Loop { get { return source.loop; } set { source.loop = value; } }
+        public float Volume { get { return source.volume; } set { source.volume = value; } }
+        public float CurrentTime { get { return source.time; } set { source.time = value; } }
+        public int timeSamples { get { return source.timeSamples; } set { source.timeSamples = value; } }
+        public AudioClip clip { get { return source.clip; } set { source.clip = value; } }
+        public Action finishOne { get; set; }
+
+        public void Play()
+        {
+            source.Play();
+            trackListener.StartListener();
+        }
+
+        public void Stop()
+        {
+            source.Stop();
+            trackListener.StopListener();
+        }
+
+        public void Pause()
+        {
+            source.Pause();
+        }
+
+        public void UnPause()
+        {
+            source.UnPause();
+        }
+
+        public void Delete()
+        {
+            if (source != null)
+            {
+                trackListener.StopListener();
+                source.gameObject.SetActive(false);
+                Destroy(source.gameObject);
+            }
+        }
+    }
+
+    [DisallowMultipleComponent]
+    public class AudioTrackListener : MonoBehaviour
+    {
+        public Action finishOne { get; set; }
+
+        public void StartListener()
+        { 
+        
+        }
+        public void StopListener()
+        { 
+        
+        }
+    }
+}
---
+++ Runtime/Components/CloneItem.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[DefaultExecutionOrder(-1000)]
+public class CloneItem : MonoBehaviour
+{
+    [Header("项目容器")]
+    [Tooltip("如果为空，自动识别此物体为容器")]
+    public Transform container;
+
+    [Header("项目预设")] public List<GameObject> itemPrefabs = new List<GameObject>();
+
+    [SerializeField, Header("当前生成的项目")] private List<GameObject> currentGenerateItems = new List<GameObject>();
+    public List<GameObject> CurrentGenerateItems { get { return currentGenerateItems; } }
+
+    private void Awake()
+    {
+        if (container == null)
+        {
+            container = transform;
+        }
+        for (int i = 0; i < itemPrefabs.Count; i++)
+        {
+            if (itemPrefabs[i] != null)
+            {
+                itemPrefabs[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public GameObject Clone(int prefabIndex = 0, bool active = true)
+    {
+        if (prefabIndex < 0 || prefabIndex >= itemPrefabs.Count)
+        {
+            return null;
+        }
+        GameObject prefab = itemPrefabs[prefabIndex];
+        if (prefab != null)
+        {
+            GameObject item = Instantiate(prefab, container);
+            item.name = prefab.name;
+            item.SetActive(active);
+            currentGenerateItems.Add(item);
+            return item;
+        }
+        return null;
+    }
+
+    public T Clone<T>(int prefabIndex = 0, bool active = true) where T : Component
+    {
+        GameObject item = Clone(prefabIndex, active);
+        if (item != null)
+        {
+            return item.GetComponent<T>();
+        }
+        return null;
+    }
+
+    public void DeleteItem(int itemIndex, bool destroyImmediate = false)
+    {
+        if (itemIndex < 0 || itemIndex >= currentGenerateItems.Count)
+        {
+            return;
+        }
+        GameObject item = currentGenerateItems[itemIndex];
+        currentGenerateItems.RemoveAt(itemIndex);
+        if (item != null)
+        {
+            item.SetActive(false);
+            if (destroyImmediate)
+            {
+                DestroyImmediate(item);
+            }
+            else
+            {
+                Destroy(item);
+            }
+        }
+    }
+
+    public void DeleteAll(bool destroyImmediate = false)
+    {
+        List<GameObject> tempList = new List<GameObject>(currentGenerateItems);
+        currentGenerateItems.Clear();
+        for (int i = 0; i < tempList.Count; i++)
+        {
+            if (tempList[i] != null)
+            {
+                tempList[i].SetActive(false);
+                if (destroyImmediate)
+                {
+                    DestroyImmediate(tempList[i]);
+                }
+                else
+                {
+                    Destroy(tempList[i]);
+                }
+            }
+        }
+    }
+
+}
