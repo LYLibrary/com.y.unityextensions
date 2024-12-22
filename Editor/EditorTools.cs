6deffd0ea3d0815f9ef0702b018a610c0c75a7ac
LYLibrary <1402609566@qq.com>
2024-12-22 13:21:00 +0800

    v1.0.0 1

---
+++ Editor/EditorTools.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorTools
+{
+    public const string MenuHead = "Y/";
+
+    [MenuItem(MenuHead + "TestMenu")]
+    private static void TestMenu()
+    {
+        UnityEngine.Debug.Log(string.Format("<color=white>{0}</color>", "测试"));
+    }
+
+}
